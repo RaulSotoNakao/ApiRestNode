99f566272548e482976f9c70a86d38395ec993e0
Raul Soto <raul_0.4_@hotmail.com>
2021-03-06 18:07:06 +0100

    prueba conexión bdd y server

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-hi;
-he2;
+const express = require("express");
+const mysql = require("mysql");
+const PORT = 8082;
+const app = express();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "negocio",
+  password: "negocionails",
+  database: "negocio",
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to my api");
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(PORT, () => console.log(`server running on port${PORT}`));
